98587df98e70284ec44968956520d1be17d3dd78
ansiabdo <ansi.abdooh@gmail.com>
2024-04-02 01:41:38 +0300

    fix amount.toString() => amount

--- lib/src/bas.dart
+++ lib/src/bas.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:js' as js;
 import 'dart:html' as html;
 
-
 import 'model/auth_code.dart';
 import 'model/sdk_config.dart';
 import 'model/transaction.dart';
@@ -11,6 +10,7 @@ import 'utils/util.dart';
 
 class BasSDK {
   static final BasSDK _singleton = new BasSDK.internal();
+
   //bool _isReady=false;
 
   factory BasSDK() {
@@ -20,19 +20,19 @@ class BasSDK {
   BasSDK.internal();
 
   Future<SDKConfig> onReady() async {
-    final completer=Completer<SDKConfig>();
+    final completer = Completer<SDKConfig>();
     //html.window.ad
     html.window.addEventListener('JSBridgeReady', (event) {
-      if(completer.isCompleted)
-        return;
+      if (completer.isCompleted) return;
       print("JSBridgeReady");
-      final messageEvent=(event as html.MessageEvent);
+      final messageEvent = (event as html.MessageEvent);
       Map<String, dynamic> data = messageEvent.data.cast<String, dynamic>();
       print("JSBridgeReady ${data.toString()}");
       completer.complete(SDKConfig.fromJson(data));
     }, true);
     return completer.future;
   }
+
   // Future<String?> currentLocale() async {
   //   final completer=Completer<String?>();
   //   jsBridgeCall("basCurrentLocale",  jsify({}),js.allowInterop((result){
@@ -44,45 +44,47 @@ class BasSDK {
   // }
 
   Future<AuthCode?> fetchAuthCode({required String clientId}) async {
-    final completer=Completer<AuthCode>();
-    jsBridgeCall("basFetchAuthCode", jsify({"clientId": clientId}),js.allowInterop((result){
-      final object=dartify(result);
+    final completer = Completer<AuthCode>();
+    jsBridgeCall("basFetchAuthCode", jsify({"clientId": clientId}),
+        js.allowInterop((result) {
+      final object = dartify(result);
       //AppLog.pdebug("----- ${object}");
       completer.complete(AuthCode.fromJson(object));
     }));
     return completer.future;
   }
-  
-  Future<Transaction?> payment({
-    required final String amount,
-    final String currency='YER',
-    required final String orderId,
-    required final String trxToken,
-    required final String appId}) async {
 
+  Future<Transaction?> payment(
+      {required final String amount,
+      final String currency = 'YER',
+      required final String orderId,
+      required final String trxToken,
+      required final String appId}) async {
     LOGW("-----BasSDK Lib START basPayment -------");
-    LOGW("amount :$amount , currency :$currency , orderId :$orderId , trxToken :$trxToken , appId :$appId");
+    LOGW(
+        "amount :$amount , currency :$currency , orderId :$orderId , trxToken :$trxToken , appId :$appId");
 
-      final completer=Completer<Transaction>();
-      jsBridgeCall("basPayment",
+    final completer = Completer<Transaction>();
+    try {
+      jsBridgeCall(
+          "basPayment",
           jsify({
-            "amount": {
-              "value":amount.toString(),
-              "currency": currency
-            },
+            "amount": {"value": amount, "currency": currency},
             "orderId": orderId,
             "trxToken": trxToken,
             "appId": appId
-          }),
-          js.allowInterop((result){
-            LOGW("-----BasSDK Lib basPayment Return result");
-            final object=dartify(result);
-            LOGW("-----BasSDK Lib basPayment result : $object");
-            completer.complete(Transaction.fromJson(object));
-          })
-      );
-      return completer.future;
+          }), js.allowInterop((result) {
+        LOGW("-----BasSDK Lib basPayment Return result");
+        final object = dartify(result);
+        LOGW("-----BasSDK Lib basPayment result : $object");
+        completer.complete(Transaction.fromJson(object));
+      }));
+    } on Exception catch (e) {
+      LOGW("-----BasSDK Lib basPayment ERROR :${e.toString()}");
+    }
+    return completer.future;
   }
+
   void dispose() {
     //headlessWebView?.dispose();
   }
