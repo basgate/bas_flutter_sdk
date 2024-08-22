4dd078225112c27ee6e702ff588ebfb8c3159536
Mushtaq Alabbasi <mushta9.3@gmail.com>
2024-08-22 21:32:48 +0300

    - update sdk. - add try cache to handle parsing errors. - add get bas super app configs. - update dart SDK version to >=2.15.0 <4.0.0

--- lib/bas_sdk.dart
+++ lib/bas_sdk.dart
@@ -1,4 +1,7 @@
 library bas_sdk;
-
+export 'src/model/auth_code.dart';
+export 'src/model/bas_super_app_configs.dart';
+export 'src/model/lunch_url.dart';
+export 'src/model/transaction.dart';
 export 'src/bas_noweb.dart'
 if (dart.library.html) 'src/bas.dart'; // VM
